958be9ebc3aa733471d5cf0484a7c5dcfc09b3c3
Rookin21 <crazynick21@gmail.com>
2023-09-25 22:49:07 +0300

    Тестовая генерация игроков из списка

--- TableTennis/MainWindow.xaml.cs
+++ TableTennis/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,20 @@ namespace TableTennis
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> list = (new string[] { "Айдар", "Александр Д.", "Александр Ф.", "Алмас", "Артем П." }).ToList(); //список
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Generation(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = list[new Random().Next(list.Count)];
+            Label2.Content = list[new Random().Next(list.Count)];
+            Label3.Content = list[new Random().Next(list.Count)];
+            Label4.Content = list[new Random().Next(list.Count)];
+            Label5.Content = list[new Random().Next(list.Count)];
         }
     }
 }
